d4a439eb038bec37669bbe8b9ad50f76d49241dd
Aashis Rimal <aashis_rimal@yahoo.com>
2018-03-07 22:53:52 +0200

    Database connection

--- index.php
+++ index.php
@@ -5,3 +5,19 @@
  * Date: 3/7/2018
  * Time: 9:27 PM
  */
+
+//define database variables, dsn is data source name
+$dsn = "mysql:host = localhost; dbname = symfony";
+$username = "root";
+$password = "";
+
+//Connect database
+try {
+    $conn = new PDO($dsn, $username, $password);
+    //Set PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully";
+}
+catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
